4f40ffc5d12a3f10a04383e8a096a7e5e0b8c99d
YuDe <davisfreeman1015@gmail.com>
2017-05-31 23:51:26 +0800

    Update R hw

--- crawler/police_stat_parser.py
+++ crawler/police_stat_parser.py
@@ -1,8 +1,40 @@
+# -*- coding: UTF-8 -*-
 import lxml.html
 
-def parse(file_path):
-	
-	pass
+paths = ['../data/cod0{}.html'.format(str(i)) for i in range(1, 31) ]
 
-if __name__ == '__main__':
+for path in paths:
+
+	try:
+		f = open(path, 'r', encoding='big5')
+		html = ''.join(f.readlines())
+		html = lxml.html.fromstring(html)
+
+		tables  = html.cssselect('table')[1:]
+
+		for it, table in enumerate(tables):
+
+			trs = table.cssselect('tr')
+			crimes = trs[0].cssselect('th')
+			cities = trs[1].cssselect('th')
+			crimes = [crime.text for crime in crimes]
+			city   = [city.text for city in cities][1]
+
+			output = open('../data/{0}-{1}.tsv'.format(city, str(it)), 'w')
+
+			# Buiid csv headers
+			crimes[0] = '時間'
+			output.write('\t'.join(crimes))
+			output.write('\n')
+
+			for tr in trs[2:]:
+				data = [ d.text if d.text != ' - ' else str(0) for d in tr ]
+				output.write('\t'.join(data))
+				output.write('\n')
+
+			output.close()
+
+	except Exception as e:
+		print(e)
+		pass
 	
